08ab8dfb89f86ce98020bc7e87ee1b21391d426b
Daniel Ring <dbr787@gmail.com>
2023-02-20 13:45:53 +1100

    add log things

--- .buildkite/ask.sh
+++ .buildkite/ask.sh
@@ -56,15 +56,18 @@ case $current_state in
     commands: 
       - "cd .buildkite"
       - "buildkite-agent artifact upload man-beard.gif"
+      
       - echo -e "--- I wrote a song for you :yellow_heart:"
       - echo -e "\033[33m... and it was called yellow\033[0m"
+      
       - "echo '--- How about GIFs?'"
       - printf '\033]1338;url='"artifact://man-beard.gif"';alt='"man-beard"'\a'
+      
       - "echo '--- How about links?'"
       - "./inline_link.sh https://www.buildkite.com"
       - "./inline_link.sh https://buildkite.com/unblock"
+      
       - "echo '--- This is a collapsed log group :white_check_mark:' && cat lorem-ipsum.txt"
-      - printf '\033]1338;url='"artifact://man-beard.gif"';alt='"man-beard"'\a\n'
       - "echo '~~~ This is a de-emphasized log group :no_entry:' && cat lorem-ipsum.txt"
       - "echo '+++ This is an expanded log group :star2:' && cat lorem-ipsum.txt"
 EOF
