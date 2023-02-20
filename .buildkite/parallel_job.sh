af9f88c68fdfd4bc6124c52d4af32620087f0562
Daniel Ring <dbr787@gmail.com>
2023-02-20 17:06:53 +1100

    log things

--- .buildkite/parallel_job.sh
+++ .buildkite/parallel_job.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo "This is parallel job $((BUILDKITE_PARALLEL_JOB+1)) of $BUILDKITE_PARALLEL_JOB_COUNT"
-
 echo -e "\033[1;35mThis is parallel job $((BUILDKITE_PARALLEL_JOB+1)) of $BUILDKITE_PARALLEL_JOB_COUNT\033[0m"
+wait 5
