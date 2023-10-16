af50951fb8afa76e0fec323a7aed388b414c444d
Daniel Ring <dbr787@gmail.com>
2023-10-16 13:14:53 +1100

    play with pipeline

--- .buildkite/begin.sh
+++ .buildkite/begin.sh
@@ -46,11 +46,16 @@ CHOICES=""
 # EOF
 # )
 
-# STEP_OUTPUT="- command: echo hello"
-STEP_OUTPUT=$(cat <<EOF
----
-steps:
-- command: echo hello
-EOF)
+SCRIPT_DIR=$(dirname "$0")
+# echo "SCRIPT_DIR: $SCRIPT_DIR"
+# WAIT_STEP=$(cat ./steps/wait.yml)
+HELLO_STEP=$(cat $SCRIPT_DIR/steps/hello.yml)
 
-echo "$STEP_OUTPUT" | buildkite-agent pipeline upload
+# STEP_OUTPUT="- command: echo hello"
+# STEP_OUTPUT=$(cat <<EOF
+# ---
+# steps:
+# - command: echo hello
+# EOF)
+
+echo "$HELLO_STEP" | buildkite-agent pipeline upload
