3450494515f960b597c2a0ef0f0aa67207545556
Daniel Ring <dbr787@gmail.com>
2023-12-13 21:38:51 +1100

    playing with hooks and ansi escape things

--- .buildkite/steps/logs/logs.sh
+++ .buildkite/steps/logs/logs.sh
@@ -29,3 +29,20 @@ echo '--- This is a collapsed log group :white_check_mark:' && cat lorem-ipsum.t
 echo '~~~ This is a de-emphasized log group :no_entry:' && cat lorem-ipsum.txt
 
 echo '+++ This is an expanded log group :star2:' && cat lorem-ipsum.txt
+
+# https://buildkite.com/docs/pipelines/managing-log-output#redacted-environment-variables
+# These variables will be redacted from logs....
+
+MY_PASSWORD="ThisIsASecret"
+MY_SECRET="ThisIsASecret"
+MY_TOKEN="ThisIsASecret"
+MY_ACCESS_KEY="ThisIsASecret"
+MY_SECRET_KEY="ThisIsASecret"
+MY_CONNECTION_STRING="ThisIsASecret" # (added in Agent v3.53.0)
+
+echo "MY_PASSWORD=$MY_PASSWORD"
+echo "MY_SECRET=$MY_SECRET"
+echo "MY_TOKEN=$MY_TOKEN"
+echo "MY_ACCESS_KEY=$MY_ACCESS_KEY"
+echo "MY_SECRET_KEY=$MY_SECRET_KEY"
+echo "MY_CONNECTION_STRING=$MY_CONNECTION_STRING"
