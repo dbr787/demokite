bd41cb0743415fc69e1d789f03947e11bc0c5b38
Daniel Ring <dbr787@gmail.com>
2023-12-14 23:35:15 +1100

    trying sed indent

--- .buildkite/steps/logs/wrapper.sh
+++ .buildkite/steps/logs/wrapper.sh
@@ -4,4 +4,4 @@
 # set -euxo pipefail # print executed commands to the terminal
 set -euo pipefail # don't print executed commands to the terminal
 
-. ./buildkite/steps/log/logs.sh | sed -e '/^---\s\|^+++\s\|^~~~\s/!s/^/  /'
+. .buildkite/steps/logs/logs.sh | sed -e '/^---\s\|^+++\s\|^~~~\s/!s/^/  /'
