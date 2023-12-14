705a6d5a088c8d37d08afa2781de584761d2ce39
Daniel Ring <dbr787@gmail.com>
2023-12-14 19:34:14 +1100

    working on colors

--- .buildkite/steps/logs/logs.sh
+++ .buildkite/steps/logs/logs.sh
@@ -52,52 +52,52 @@ echokite () {
 # echo -e "--- \033[93mI wrote a song for you\033[0m :yellow_heart: :guitar:";
 echo -e "--- :rainbow: $(echokite "Expand this group to see text styling options" magenta none underline)"
 echo ""
-echo "With Buildkite logs, there are 13 different text colors to choose from..."
-echokite "  01. We have black text" black none normal
-echokite "  02. We have red text" red none normal
-echokite "  03. We have green text" green none normal
-echokite "  04. We have yellow text" yellow none normal
-echokite "  05. We have blue text" blue none normal
-echokite "  06. We have magenta text" magenta none normal
-echokite "  07. We have cyan text" cyan none normal
-echokite "  08. We have white text" white none normal
-echokite "  09. We have bright black text" bright_black none normal
-echokite "  10. We have bright red text" bright_red none normal
-echokite "  11. We have bright green text" bright_green none normal
-echokite "  12. We have bright yellow text" bright_yellow none normal
-echokite "  13. We have bright blue text" bright_blue none normal
+echo "  With Buildkite logs, there are 13 different text colors to choose from..."
+echokite "    01. We have black text" black none normal
+echokite "    02. We have red text" red none normal
+echokite "    03. We have green text" green none normal
+echokite "    04. We have yellow text" yellow none normal
+echokite "    05. We have blue text" blue none normal
+echokite "    06. We have magenta text" magenta none normal
+echokite "    07. We have cyan text" cyan none normal
+echokite "    08. We have white text" white none normal
+echokite "    09. We have bright black text" bright_black none normal
+echokite "    10. We have bright red text" bright_red none normal
+echokite "    11. We have bright green text" bright_green none normal
+echokite "    12. We have bright yellow text" bright_yellow none normal
+echokite "    13. We have bright blue text" bright_blue none normal
 echo ""
-echo "There are also 3 different background colors..."
-echo -e "  $(echokite "01. We have red background" white red normal)"
-echo -e "  $(echokite "02. We have green background" black green normal)"
-echo -e "  $(echokite "03. We have gray background" white gray normal)"
+echo "  There are also 3 different background colors..."
+echo -e "    $(echokite "01. We have red background" white red normal)"
+echo -e "    $(echokite "02. We have green background" black green normal)"
+echo -e "    $(echokite "03. We have gray background" white gray normal)"
 echo ""
-echo "And we can style text with 5 different variations..."
-echo -e "  $(echokite "01. We have normal style" white none normal)"
-echo -e "  $(echokite "02. We have italic style" white none italic)"
-echo -e "  $(echokite "03. We have underlined style" white none underline)"
-echo -e "  $(echokite "04. We have blinking style" white none blink)"
-echo -e "  $(echokite "05. We have striked style" white none strike)"
+echo "  And we can style text with 5 different variations..."
+echo -e "    $(echokite "01. We have normal style" white none normal)"
+echo -e "    $(echokite "02. We have italic style" white none italic)"
+echo -e "    $(echokite "03. We have underlined style" white none underline)"
+echo -e "    $(echokite "04. We have blinking style" white none blink)"
+echo -e "    $(echokite "05. We have striked style" white none strike)"
 echo ""
 str01=$(echokite "All of these" bright_green red normal)
 str02=$(echokite "options and styles" magenta gray italic)
 str03=$(echokite "can be used" bright_red green underline)
 str04=$(echokite "in any combination" bright_blue none blink)
 str05=$(echokite "you desire!" cyan none strike)
-echo -e "$str01 $str02 $str03 $str04 $str05"
+echo -e "  $str01 $str02 $str03 $str04 $str05"
 echo ""
 echo -e "--- :link: $(echokite "Expand this group to see some example links" bright_green none underline)"
 echo ""
-printf '\033]1339;url='"https://www.buildkite.com/"';content='"Links will always be this color, and will show an underline on hover"'\a\n'
-printf '\033]1339;url='"https://www.buildkite.com/"'\a\n'
+printf '  \033]1339;url='"https://www.buildkite.com/"';content='"Links will always be this color, and will show an underline on hover"'\a\n'
+printf '  \033]1339;url='"https://www.buildkite.com/"'\a\n'
 link01="We can also include a link"
 link02='\033]1339;url='"https://www.buildkite.com/"';content='"only on certain words"'\a'
 link03="in a line of text"
-echo -e "$link01 $link02 $link03"
+echo -e "  $link01 $link02 $link03"
 echo ""
 echo -e "--- :frame_with_picture: $(echokite "But what about GIFs? I really like GIFs!" cyan none underline)"
 echo ""
-printf '\033]1338;url='"artifact://man.gif"';alt='"man"'\t\a\n'
+printf '  \033]1338;url='"artifact://man.gif"';alt='"man"'\t\a\n'
 
 
 # echo -e "\033[30m01. black (FG30)\033[0m"
