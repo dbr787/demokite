a153211658cdecfadced0065b9cc39347755a08d
Daniel Ring <dbr787@gmail.com>
2023-12-15 16:14:31 +1100

    start annotations

--- .buildkite/demokite.sh
+++ .buildkite/demokite.sh
@@ -76,12 +76,35 @@ STEP_LOGS=$(cat .buildkite/steps/logs/logs.yml)
 
 # touch pipeline_upload.yml
 
+
+pupload () {
+    local source_dir="$1"
+    local source_file="$2"
+    local output_dir="$3"
+    local output_file="$4"
+    local current_dir=$(pwd)
+
+    cd "$source_dir"
+    buildkite-agent pipeline upload "$source_file" --dry-run --format json > "$output_dir\\$output_file"
+    # buildkite-agent pipeline upload .buildkite/steps/logs/logs.yml --dry-run --format json > step_logs.json
+    cd "$output_dir"
+    buildkite-agent artifact upload "$output_file" --log-level error
+    cd "$current_dir"
+
+}
+
+pupload ".buildkite/steps/logs" "logs.yml" "." "logs.json"
+
+
+
+
 # echo "$STEP_LOGS"
 # echo "$STEP_LOGS" >> step_logs.yml
 # cat pipeline_upload.yml
 # echo "$STEP_LOGS" | buildkite-agent pipeline upload
-buildkite-agent pipeline upload .buildkite/steps/logs/logs.yml --dry-run --format json > step_logs.json
-buildkite-agent artifact upload step_logs.json --log-level error;
+# buildkite-agent pipeline upload .buildkite/steps/logs/logs.yml --dry-run --format json > step_logs.json
+# buildkite-agent artifact upload step_logs.json --log-level error;
+
 
 
 
@@ -89,23 +112,26 @@ buildkite-agent artifact upload step_logs.json --log-level error;
 # echo "$STEP_ANNOTATIONS" >> pipeline_upload.yml
 # cat pipeline_upload.yml
 # echo "$STEP_ANNOTATIONS" | buildkite-agent pipeline upload
-buildkite-agent pipeline upload .buildkite/steps/annotations/annotations.yml --dry-run --format json > step_annotations.json
-buildkite-agent artifact upload step_annotations.json --log-level error;
+# buildkite-agent pipeline upload .buildkite/steps/annotations/annotations.yml --dry-run --format json > step_annotations.json
+# buildkite-agent artifact upload step_annotations.json --log-level error;
+
+# jq -s 'reduce .[] as $file ([]; . + $file.steps)' *.json > merged.json
+# buildkite-agent artifact upload step_annotations.json --log-level error;
 
 
 
-mv pipeline_upload.yml pipeline_upload_original.yml
+# mv pipeline_upload.yml pipeline_upload_original.yml
 
-buildkite-agent artifact upload "pipeline_upload_original.yml" --log-level error;
+# buildkite-agent artifact upload "pipeline_upload_original.yml" --log-level error;
 
 # sanitises pipeline upload file
 # just remove '---' and 'steps:' lines for now, might need to adjust this later to allow env: and agents: properties
 # remove all but first occurence of '---' and 'steps:'
 # sed -e '1!{/^---/d}' -e '/^[[:space:]]*steps:[[:space:]]*/d' pipeline_upload_original.yml > pipeline_upload_sanitised.yml
-sed '/^[[:space:]]*---[[:space:]]*/d; /^[[:space:]]*steps:[[:space:]]*/d' pipeline_upload_original.yml > pipeline_upload_sanitised.yml
+# sed '/^[[:space:]]*---[[:space:]]*/d; /^[[:space:]]*steps:[[:space:]]*/d' pipeline_upload_original.yml > pipeline_upload_sanitised.yml
 
 # upload pipeline_upload.yml as artifact
-buildkite-agent artifact upload "pipeline_upload_sanitised.yml" --log-level error;
+# buildkite-agent artifact upload "pipeline_upload_sanitised.yml" --log-level error;
 
 # pipeline upload
-cat pipeline_upload_sanitised.yml | buildkite-agent pipeline upload
+# cat pipeline_upload_sanitised.yml | buildkite-agent pipeline upload
