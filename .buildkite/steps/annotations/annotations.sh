ac47d6fcb76f49937fe87a87b97a2f5e6ba35bbe
Daniel Ring <dbr787@gmail.com>
2023-12-20 12:08:16 +1100

    add log children bk emojis not working text

--- .buildkite/steps/annotations/annotations.sh
+++ .buildkite/steps/annotations/annotations.sh
@@ -108,11 +108,14 @@ line 1
 
 This annotation was created by job: <a href=\"${BUILDKITE_BUILD_URL}#$BUILDKITE_JOB_ID\">$BUILDKITE_LABEL</a>
 
+The file used to generate this annotation was uploaded as <a href=\"artifact://wiz-docker-scan-annotation.md\">an artifact which can be found here</a>
+
 \`\`\`term
 Results of the scan go \033[0;31mhere\033[0m
 \`\`\`
 
-line 2  
+line 2
+
 </details>
 " | buildkite-agent annotate --context "ctx-wiz-docker-scan" --style "$ANNOTATION_STYLE"
 
