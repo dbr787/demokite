5b4f7e5a7b57a802e40bbc13b5816d3e1c048180
Daniel Ring <dbr787@gmail.com>
2024-05-24 14:25:34 +1000

    try adding rows in script

--- .buildkite/steps/deploy-status/deploy-status.sh
+++ .buildkite/steps/deploy-status/deploy-status.sh
@@ -17,9 +17,14 @@ cd .buildkite/steps/deploy-status/;
 # replace variables in annotation file
 FILE_PATH="./assets/example01.md"
 
+ROW_1="<tr> <td>Bison</td> <td>Development</td> <td>1a1e395</td> <td>1a1e395</td> <td class=\"bold green\">Successful</td> <td>$TIMESTAMP</td> </tr>"
+ROW_2="<tr> <td>Bison</td> <td>Development</td> <td>1a1e395</td> <td>1a1e395</td> <td class=\"bold green\">Successful</td> <td>$TIMESTAMP</td> </tr>"
+ROW_3="<tr> <td>Bison</td> <td>Development</td> <td>1a1e395</td> <td>1a1e395</td> <td class=\"bold green\">Successful</td> <td>$TIMESTAMP</td> </tr>"
+
 replace_file_var $FILE_PATH "\$BUILDKITE_BUILD_URL" "$BUILDKITE_BUILD_URL"
 replace_file_var $FILE_PATH "\$BUILDKITE_JOB_ID" "$BUILDKITE_JOB_ID"
 replace_file_var $FILE_PATH "\$BUILDKITE_LABEL" "$BUILDKITE_LABEL"
+replace_file_var $FILE_PATH "\$ROW_1" "$ROW_1"
 
 # annotate
 # buildkite-agent annotate 'Example `error` style annotation' --style 'error' --context 'ctx-error'
@@ -28,6 +33,11 @@ replace_file_var $FILE_PATH "\$BUILDKITE_LABEL" "$BUILDKITE_LABEL"
 # buildkite-agent annotate 'Example `info` style annotation' --style 'info' --context 'ctx-info'
 
 printf '%b\n' "$(cat $FILE_PATH)" | buildkite-agent annotate --style 'info' --context 'example'
+
+sleep 5;
+
+
+
 
 # upload assets as artifacts
 buildkite-agent artifact upload "assets/*" --log-level error;
