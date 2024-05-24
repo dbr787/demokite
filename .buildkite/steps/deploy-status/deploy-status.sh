75b339482272a4fa1d1e3a450ce03846d8e802bb
Daniel Ring <dbr787@gmail.com>
2024-05-24 15:22:11 +1000

    dynamic annotation

--- .buildkite/steps/deploy-status/deploy-status.sh
+++ .buildkite/steps/deploy-status/deploy-status.sh
@@ -22,7 +22,7 @@ TEMP_FILE_PATH="./assets/example01-$EPOCH.md"
 cp "$FILE_PATH" "$TEMP_FILE_PATH"
 
 TIMESTAMP=$(date)
-ROW_1="<tr> <td>Bison</td> <td>Development</td> <td>1a1e395</td> <td>1a1e395</td> <td class=\"bold orange\">:party_parrot: In Progress</td> <td>$TIMESTAMP</td> </tr>"
+ROW_1="<tr> <td>Bison</td> <td>Development</td> <td>1a1e395</td> <td>1a1e395</td> <td class=\"bold orange\">:partyparrot: In Progress</td> <td>$TIMESTAMP</td> </tr>"
 ROW_2="<tr> <td>Bison</td> <td>Test</td> <td>1a1e395</td> <td>1a1e395</td> <td class=\"bold gray\">Waiting</td> <td>$TIMESTAMP</td> </tr>"
 ROW_3="<tr> <td>Bison</td> <td>Production</td> <td>1a1e395</td> <td>1a1e395</td> <td class=\"bold gray\">Waiting</td> <td>$TIMESTAMP</td> </tr>"
 
