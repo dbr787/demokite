bcaec6df9109a17641df1e50f5b4a623247ec612
Daniel Ring <dbr787@gmail.com>
2024-05-25 23:41:25 +1000

    improve annotation method;

--- .buildkite/demokite.sh
+++ .buildkite/demokite.sh
@@ -1,144 +1,91 @@
 #!/bin/bash
 
 # set explanation: https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425
-# set -euxo pipefail # print executed commands to the terminal
-set -euo pipefail # don't print executed commands to the terminal
+# set -euo pipefail # don't print executed commands to the terminal
+set -euo pipefail
 
 # source shared functions
-. .buildkite/assets/functions.sh;
+. .buildkite/assets/functions.sh
 
-CHOICE_ANNOTATIONS=""
-CHOICE_LOGS=""
-CHOICE_PARALLELISM=""
-CHOICE_BUILD_PASS=""
-CHOICE_BUILD_FAIL=""
-CHOICES=""
+# capture original working directory
+current_dir=$(pwd)
+current_dir_contents=$(ls -lah "$current_dir")
 
-CURRENT_STATE=""
-FIRST_STEP_KEY="begin"
-CURRENT_DIR=$(pwd)
+# change into step directory
+cd .buildkite/steps/deploy-status/
 
-pipeline_prepare ".buildkite/steps/deploy-status" "deploy-status.yml" $CURRENT_DIR "deploy-status.json"
-pipeline_prepare ".buildkite/steps/ask" "ask.yml" $CURRENT_DIR "ask.json"
-pipeline_merge "deploy-status.json" "ask.json" > "merged.json"
-# artifact_upload "merged.json"
-pipeline_upload "merged.json"
+# Function to escape special characters for sed
+escape_string() {
+    printf '%s\n' "$1" | sed -e 's/[\/&]/\\&/g' -e 's/"/\\"/g'
+}
 
-# if [ "$BUILDKITE_STEP_KEY" != "$FIRST_STEP_KEY" ]; then
-#   CURRENT_STATE=$(buildkite-agent meta-data get "choice")
-#   echo "BUILDKITE_STEP_KEY: $BUILDKITE_STEP_KEY"
-#   echo "CHOICE: $CURRENT_STATE"
-#   if [ $CURRENT_STATE = "logs" ]; then
-#     pipeline_prepare ".buildkite/steps/logs" "logs.yml" $CURRENT_DIR "logs.json"
-#     pipeline_prepare ".buildkite/steps/ask" "ask.yml" $CURRENT_DIR "ask.json"
-#     pipeline_merge "logs.json" "ask.json" > "merged.json"
-#     # artifact_upload "merged.json"
-#     pipeline_upload "merged.json"
-#   fi
-#   if [ $CURRENT_STATE = "annotations" ]; then
-#     pipeline_prepare ".buildkite/steps/annotations" "annotations.yml" $CURRENT_DIR "annotations.json"
-#     pipeline_prepare ".buildkite/steps/ask" "ask.yml" $CURRENT_DIR "ask.json"
-#     pipeline_merge "annotations.json" "ask.json" > "merged.json"
-#     # artifact_upload "merged.json"
-#     pipeline_upload "merged.json"
-#   fi
-#   if [ $CURRENT_STATE = "parallel-steps" ]; then
-#     pipeline_prepare ".buildkite/steps/parallel-steps" "parallel-steps.yml" $CURRENT_DIR "parallel-steps.json"
-#     pipeline_prepare ".buildkite/steps/ask" "ask.yml" $CURRENT_DIR "ask.json"
-#     pipeline_merge "parallel-steps.json" "ask.json" > "merged.json"
-#     # artifact_upload "merged.json"
-#     pipeline_upload "merged.json"
-#   fi
-#   if [ $CURRENT_STATE = "retry" ]; then
-#     pipeline_prepare ".buildkite/steps/retry" "retry.yml" $CURRENT_DIR "retry.json"
-#     pipeline_prepare ".buildkite/steps/ask" "ask.yml" $CURRENT_DIR "ask.json"
-#     pipeline_merge "retry.json" "ask.json" > "merged.json"
-#     # artifact_upload "merged.json"
-#     pipeline_upload "merged.json"
-#   fi
-#   if [ $CURRENT_STATE = "deploy-status" ]; then
-#     pipeline_prepare ".buildkite/steps/deploy-status" "deploy-status.yml" $CURRENT_DIR "deploy-status.json"
-#     pipeline_prepare ".buildkite/steps/ask" "ask.yml" $CURRENT_DIR "ask.json"
-#     pipeline_merge "deploy-status.json" "ask.json" > "merged.json"
-#     # artifact_upload "merged.json"
-#     pipeline_upload "merged.json"
-#   fi
-#   if [ $CURRENT_STATE = "pass" ]; then
-#     pipeline_prepare ".buildkite/steps/pass" "pass.yml" $CURRENT_DIR "pass.json"
-#     pipeline_upload "pass.json"
-#   fi
-#   if [ $CURRENT_STATE = "fail" ]; then
-#     pipeline_prepare ".buildkite/steps/fail" "fail.yml" $CURRENT_DIR "fail.json"
-#     pipeline_upload "fail.json"
-#   fi
-# else
-#   sleep 10
-#   pipeline_prepare ".buildkite/steps/ask" "ask.yml" $CURRENT_DIR "ask.json"
-#   # artifact_upload "ask.json"
-#   pipeline_upload "ask.json"
-# fi
+# Function to update file content
+update_file() {
+    local template_file="./assets/template.html"
+    local output_file="./assets/annotation.html"
+    local new_title="$1"
+    local new_subtitle="$2"
+    local application="$3"
+    local environment="$4"
+    local deployed_version="$5"
+    local new_version="$6"
+    local deployment_status="$7"
+    local deployment_progress="$8"
+    local last_updated="$9"
+    local buildkite_job="${10}"
+    local application_link="${11}"
 
+    # Check if the template file exists
+    if [[ ! -f "$template_file" ]]; then
+        echo "Template file not found!"
+        return 1
+    fi
 
+    # If annotation.html does not exist, create it from the template
+    if [[ ! -f "$output_file" ]]; then
+        cp "$template_file" "$output_file"
+    fi
 
-# STEP_OUTPUT=$(cat <<EOF
-#   - label: ":thumbsup: Pass build"
-#     command: "echo 'Exiting build with status 0' && exit 0"
-# EOF
-# )
+    # Create the new table row
+    local new_table_row="<tr>
+        <td>${application}</td>
+        <td>${environment}</td>
+        <td>${deployed_version}</td>
+        <td>${new_version}</td>
+        <td>${deployment_status}</td>
+        <td>${deployment_progress}</td>
+        <td>${last_updated}</td>
+        <td>${buildkite_job}</td>
+        <td><a href=\"${application_link}\">Link</a></td>
+    </tr>"
 
-# sed -e '/---/d' -e '/steps:/d' -e '/^$/d' .buildkite/steps/wait.yml
+    # Debug: Echo the replacements to ensure correctness
+    echo "Replacing {{title}} with ${new_title}"
+    echo "Replacing {{subtitle}} with ${new_subtitle}"
+    echo "Replacing {{table_rows}} with ${new_table_row}"
 
+    # Use awk to update the contents of the annotation.html file
+    awk -v title="$new_title" \
+        -v subtitle="$new_subtitle" \
+        -v table_rows="$new_table_row" \
+        '{
+            gsub(/{{title}}/, title);
+            gsub(/{{subtitle}}/, subtitle);
+            gsub(/{{table_rows}}/, table_rows);
+            print
+        }' "$output_file" > "${output_file}.tmp" && mv "${output_file}.tmp" "$output_file"
 
+    # Create the timestamped backup of the updated annotation.html
+    local dir_path
+    local file_name
+    dir_path=$(dirname "$output_file")
+    file_name=$(basename "$output_file" .html)
+    local timestamp
+    timestamp=$(date +%Y%m%d%H%M%S)
+    local timestamped_file="${dir_path}/${file_name}_${timestamp}.html"
+    cp "$output_file" "$timestamped_file"
+    
+    echo "Output file updated successfully. Timestamped file created at $timestamped_file"
+}
 
-# merge multiple json pipeline definitions into one and upload
-# current_dir=$(pwd)
-# pipeline_prepare ".buildkite/steps/logs" "logs.yml" $current_dir "logs.json"
-# pipeline_prepare ".buildkite/steps/annotations" "annotations.yml" $current_dir "annotations.json"
-# pipeline_merge "logs.json" "annotations.json" > "merged.json"
-# artifact_upload "merged.json"
-# pipeline_upload "merged.json"
-
-
-# here is where im working 
-# pipeline_prepare ".buildkite/steps/ask" "ask.yml" $current_dir "ask.json"
-# artifact_upload "ask.json"
-# pipeline_upload "ask.json"
-
-
-# echo "$STEP_LOGS"
-# echo "$STEP_LOGS" >> step_logs.yml
-# cat pipeline_upload.yml
-# echo "$STEP_LOGS" | buildkite-agent pipeline upload
-# buildkite-agent pipeline upload .buildkite/steps/logs/logs.yml --dry-run --format json > step_logs.json
-# buildkite-agent artifact upload step_logs.json --log-level error;
-
-
-
-
-# echo "$STEP_ANNOTATIONS"
-# echo "$STEP_ANNOTATIONS" >> pipeline_upload.yml
-# cat pipeline_upload.yml
-# echo "$STEP_ANNOTATIONS" | buildkite-agent pipeline upload
-# buildkite-agent pipeline upload .buildkite/steps/annotations/annotations.yml --dry-run --format json > step_annotations.json
-# buildkite-agent artifact upload step_annotations.json --log-level error;
-
-# jq -s 'reduce .[] as $file ([]; . + $file.steps)' *.json > merged.json
-# buildkite-agent artifact upload step_annotations.json --log-level error;
-
-
-
-# mv pipeline_upload.yml pipeline_upload_original.yml
-
-# buildkite-agent artifact upload "pipeline_upload_original.yml" --log-level error;
-
-# sanitises pipeline upload file
-# just remove '---' and 'steps:' lines for now, might need to adjust this later to allow env: and agents: properties
-# remove all but first occurence of '---' and 'steps:'
-# sed -e '1!{/^---/d}' -e '/^[[:space:]]*steps:[[:space:]]*/d' pipeline_upload_original.yml > pipeline_upload_sanitised.yml
-# sed '/^[[:space:]]*---[[:space:]]*/d; /^[[:space:]]*steps:[[:space:]]*/d' pipeline_upload_original.yml > pipeline_upload_sanitised.yml
-
-# upload pipeline_upload.yml as artifact
-# buildkite-agent artifact upload "pipeline_upload_sanitised.yml" --log-level error;
-
-# pipeline upload
-# cat pipeline_upload_sanitised.yml | buildkite-agent pipeline upload
+update_file "New Title" "New Subtitle" "App1" "Env1" "1.0" "1.1" "Success" "100%" "2024-05-25" "Job1" "http://example.com"
