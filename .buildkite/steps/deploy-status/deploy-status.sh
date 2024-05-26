38f55d1a498e0ec60c3125441f1277177e22d43b
Daniel Ring <dbr787@gmail.com>
2024-05-26 17:04:00 +1000

    improve annotation method

--- .buildkite/steps/deploy-status/deploy-status.sh
+++ .buildkite/steps/deploy-status/deploy-status.sh
@@ -199,11 +199,6 @@ update_files() {
     echokite "JSON file updated successfully: $json_output_file" green none normal
     echokite "Timestamped backup created at: $timestamped_file" green none normal
 
-    # # Run the buildkite-agent annotate command with style and context if style is different
-    # if [[ -n "$style" && "$style" != "$current_style" ]]; then
-    #     buildkite-agent annotate --style "$style" --context "$context"
-    # fi
-
     # Check if the template HTML file exists
     if [[ ! -f "$html_template_file" ]]; then
         echokite "Template HTML file not found!" red none normal
@@ -246,6 +241,11 @@ update_files() {
 
     # Pipe the contents of the final HTML file to the buildkite-agent annotate command
     echo "$html_content" | buildkite-agent annotate --style "$style" --context "$context"
+
+    # If style is provided and different from current style, update it separately
+    if [[ -n "$style" && "$style" != "$current_style" ]]; then
+        buildkite-agent annotate --style "$style" --context "$context" --append
+    fi
 }
 
 update_files \
