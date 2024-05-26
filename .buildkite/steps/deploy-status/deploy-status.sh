4c63638c6cfe091a8af1231ce9ab421a2fee1715
Daniel Ring <dbr787@gmail.com>
2024-05-26 11:13:30 +1000

    improve annotation method

--- .buildkite/steps/deploy-status/deploy-status.sh
+++ .buildkite/steps/deploy-status/deploy-status.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-set -euo pipefail
+# set explanation: https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425
+# set -euxo pipefail # print executed commands to the terminal
+set -euo pipefail # don't print executed commands to the terminal
+
+# source shared functions
+. .buildkite/assets/functions.sh;
+
+# capture original working directory
+current_dir=$(pwd)
+current_dir_contents=$(ls -lah $current_dir)
+
+# change into step directory
+cd .buildkite/steps/deploy-status/;
 
 # Function to update the JSON file
 update_json() {
