d9575ebb23df6225f45f809518b3f3c5d871da9e
Daniel Ring <dbr787@gmail.com>
2024-05-27 18:30:15 +1000

    another lazy commit

--- .buildkite/steps/deploy-new/deploy-new.sh
+++ .buildkite/steps/deploy-new/deploy-new.sh
@@ -26,11 +26,11 @@ sleep 5;
 
 # update values in the json file
 deployment_key="deployments.bison-dev"
-update_deployment --key "$deployment_key.old_version.text" --value "$previous_commit" --debug "debug";
-update_deployment --key "$deployment_key.new_version.text" --value "$current_commit";
-update_deployment --key "$deployment_key.deployment_strategy.text" --value "Canary";
-update_deployment --key "$deployment_key.deployment_progress.text" --value ":large_green_circle::white_circle::white_circle::white_circle::white_circle:";
-update_deployment --key "$deployment_key.deployment_status.text" --value "Updated Status Text" ;
+update_json --key "$deployment_key.old_version.text" --value "$previous_commit" --debug "debug";
+update_json --key "$deployment_key.new_version.text" --value "$current_commit";
+update_json --key "$deployment_key.deployment_strategy.text" --value "Canary";
+update_json --key "$deployment_key.deployment_progress.text" --value ":large_green_circle::white_circle::white_circle::white_circle::white_circle:";
+update_json --key "$deployment_key.deployment_status.text" --value "Updated Status Text" ;
 
 # update the annotation
 update_annotation --debug "debug";
